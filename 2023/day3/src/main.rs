0164ca33f5b2ed025b4e88d3f93febad887726c4
Phydrogen <coeffiergabriel@gmail.com>
2023-12-03 20:42:04 +0100

    basic pass but not final

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,37 +1,57 @@
-use std::fs;
+use std::{fs, collections::HashSet};
 
 fn main() {
     let symbol = ['$','*','+','-','=','%','@','/','#'];
     let mut bomb = vec![];
     
-    let file = fs::read_to_string("input_test").unwrap();
+    let file = fs::read_to_string("input").unwrap();
     for (index, line) in file.lines().enumerate() {
         for &sy in symbol.iter() {
             if line.contains(sy) && index > 0 {
                 let pos = line.find(sy).unwrap();
-                println!("find {sy} at {index}:{pos}");
-                bomb.push((index, pos));
+                print!("{sy} at {index}:{pos}  |");
+                bomb.push((index as i32, pos as i32));
             }
         }
     }
     
     let mut pos_nb = vec![];
 
-    for line in file.lines() {
+    for (line_count, line) in file.lines().enumerate() {
         let mut number = String::new();
 
         for (j, l) in line.chars().enumerate() {
             if l.is_ascii_digit() {
-                print!("{l}");
                 number = format!("{number}{l}");
             }
-            if l == '.' && !number.is_empty() {
+            if !l.is_ascii_digit() && !number.is_empty() {
                 let nb = number.parse::<usize>().unwrap();
-                pos_nb.push((j - number.len(), j, nb));
+                pos_nb.push((line_count as i32, (j - number.len()) as i32, j as i32, nb));
                 number = String::new();
             }
         }
     }
-    println!("possible: {pos_nb:?}")
+    println!("{pos_nb:?}");
+    let mut jspfrr: HashSet<usize> = HashSet::new();
+    for (ligne_signe, debut_signe) in bomb {
+        for (ligne_chiffre, debut_chiffre, fin_chiffre, leditchiffre) in pos_nb.iter() {
+            if ligne_signe == *ligne_chiffre {
+                if debut_signe == debut_chiffre - 1 {
+                    jspfrr.insert(leditchiffre.to_owned());
+                }
+                if debut_signe == *fin_chiffre {
+                    jspfrr.insert(leditchiffre.to_owned());
+                }
+            }
+            if ligne_signe - 1 == *ligne_chiffre && debut_signe >= *debut_chiffre - 1 && debut_signe <= *fin_chiffre + 1 {
+                jspfrr.insert(leditchiffre.to_owned());
+            }
+            if ligne_signe + 1 == *ligne_chiffre && debut_signe >= *debut_chiffre - 1 && debut_signe <= *fin_chiffre + 1 {
+                jspfrr.insert(leditchiffre.to_owned());
+            }
+        }
+    }
+    let su: usize = jspfrr.iter().sum();
+    println!("{:?}", su);
 
 }
