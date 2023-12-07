fc7e355e0255d1996312627d5ad05ced676eb7ed
Phydrogen <coeffiergabriel@gmail.com>
2023-12-07 11:13:27 +0100

    feat: part2 done was very very close

--- 2023/day4/src/main.rs
+++ 2023/day4/src/main.rs
@@ -1,9 +1,10 @@
-use std::{fs, collections::HashMap};
+use std::{fs, collections::HashMap, vec};
 
 fn main() {
     let file = fs::read_to_string("input").unwrap();
     let mut memory = HashMap::new();
     let mut _p1 = 0;
+    let mut oricard: Vec<u32> = vec![];
     for line in file.lines() {
         let (card_id, other_line) = line.split_once(':').unwrap();
         let id: u32 = card_id.strip_prefix("Card ").unwrap().trim().parse::<u32>().unwrap();
@@ -11,26 +12,20 @@ fn main() {
         let nb: Vec<u32> = winning.trim().split(' ').filter(|x| !x.is_empty()).map(|c| c[0..c.len()].parse().unwrap()).collect();
         let my_nb: Vec<u32> = mycard.trim().split(' ').filter(|x| !x.is_empty()).map(|c| c[0..c.len()].parse().unwrap()).collect();
         let mut card_point: u32 = 0;
-        
+        oricard.push(id);
         for leditnombre in my_nb {
             if let Some(_x) = nb.iter().position(|&x| x == leditnombre) {
                 card_point += 1;
             }
         }
         for n in id+1..=id+card_point {
-            if id == 1 {
-                memory.insert(1, 0);
-            }
-            if let Some(&v) = memory.get(&n) {
-                let mult = memory.get(&id).unwrap();
-                memory.insert(n,  mult + v + 1);
+            if let Some(&mult) = memory.get(&id) {
+                *memory.entry(n).or_insert(0) += mult + 1;
             } else {
                 memory.insert(n, 1);
             }
         };
-        if card_point == 0 && !memory.contains_key(&id) {
-            memory.insert(id, 0);
-        }
+
         println!("{memory:?}");
 
         if card_point > 1 {
@@ -40,7 +35,7 @@ fn main() {
         _p1 += card_point;
     }
     // println!("total : {p1}");
-    println!("memory : {:?}", memory.values().sum::<usize>() + memory.len());
+    println!("memory : {:?}", memory.values().sum::<usize>() + oricard.len());
 
 }
 
