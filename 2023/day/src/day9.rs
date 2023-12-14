5f1c74a402b5d61758806af9d467d27576cd1f28
Phydrogen <coeffiergabriel@gmail.com>
2023-12-14 20:42:41 +0100

    feat: start part 1 and doing recursive

--- 2023/day/src/day9.rs
+++ 2023/day/src/day9.rs
@@ -1,8 +1,33 @@
 use aoc_runner_derive::aoc;
+use rayon::iter::IndexedParallelIterator;
 
 #[aoc(day9, part1)]
 fn part_1(file: &str) -> Option<i32> {
+
+    for arr in file_parser(file) {
+        walker(arr, 0);
+    }
+
     None
+}
+fn walker(arr: Vec<i32>, cpt: i32) -> i32 {
+    let mut v = vec![];
+
+    if arr.iter().sum::<i32>() == 0 {
+        return *arr.last().unwrap();
+    }
+
+    for idx in (0..arr.len() - 1).step_by(2) {
+        v.push(arr[idx] - arr[idx+1])
+    }
+    println!("recur !");
+    walker(v, cpt) + cpt
+}
+
+fn file_parser(file: &str) -> Vec<Vec<i32>> {
+    file.lines().map(|f| {
+        f.split(' ').map(|e| e.parse::<i32>().unwrap()).collect::<Vec<i32>>()
+    }).collect()
 }
 
 #[aoc(day9, part2)]
@@ -14,11 +39,13 @@ fn part_2(file: &str) -> Option<i32> {
 mod test {
 
     use super::*;
-    const EXMPL: &str = "";
+    const EXMPL: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(EXMPL), Some(0));
+        assert_eq!(part_1(EXMPL), Some(114));
     }
 
 
