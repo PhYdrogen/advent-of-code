ad3d7bc31129484f370a927df1733d05008b5e14
Phydrogen <coeffiergabriel@gmail.com>
2023-12-15 21:48:34 +0100

    feat: added skeleton

---
+++ 2023/day/src/dayXX.rs
@@ -0,0 +1,31 @@
+use aoc_runner_derive::aoc;
+
+#[aoc(day, part)]
+fn part_1(file: &str) -> Option<i32> {
+    None
+}
+
+#[aoc(day, part)]
+fn part_2(file: &str) -> Option<i32> {
+    None
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    const EXMPL: &str = "";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(EXMPL), Some(0));
+    }
+
+
+    // Part2 is not optimize for brute force (normal way)
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(EXMPL), Some(0));
+
+    }
+}
